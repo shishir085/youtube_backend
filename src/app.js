7a6d7aa44c8406fd5271c33d2f1387d93f9a2d3c
Shishir Pandey <shishirpandey087@gmail.com>
2025-09-14 23:09:18 +0545

    added async await

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app=express()
+
+import dotenv from "dotenv"
+dotenv.config()
+
+app.get()
+
+app.listen()
--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+import { DB_NAME } from "./constants"
 
+
+import express from "express";
+
+const app=express()
+// iffe
+
+(async () => {
+    try {
+        await
+            mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+            app.on("error",(error)=>{
+                    
+            })
+    } catch (error) {
+        console.error(error);
+        throw err;
+    }
+})();
