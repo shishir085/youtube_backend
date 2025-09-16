cc3337f6778d4f5bcfebb50054d4caa3ba1b6c4f
Shishir Pandey <shishirpandey087@gmail.com>
2025-09-16 22:31:16 +0545

    added index.js in db folder

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config()
-import { DB_NAME } from "./constants.js"
 
 
-
-    ;const connectDB= (async () => {
-
-        try {
-            const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME} `)
-            console.log(`\n mongo db connected !! db host ${connectionInstance.connection.host}`);
-
-        } catch (error) {
-            console.log(error);
-
-        }
-    })()
-
-    export default connectDB
