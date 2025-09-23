a4c9cb9917fd558649479a61e30f64cd1e7f6db3
Shishir Pandey <shishirpandey087@gmail.com>
2025-09-23 10:49:29 +0545

    for checking

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ dotenv.config();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -25,11 +26,18 @@ app.use(express.json({ limit: "16kb" }));
 
 app.use(cookieParser());
 
-// routes import
-import userRouter from "./routes/user.routes.js";
 
-// routes declaration
 
-app.use("/api/v1/users", userRouter);
+// routes import 
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration 
+
+
+app.use("/api/v1/users",userRouter)
+
+
+
 
 export { app };
